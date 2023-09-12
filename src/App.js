a9a79e009a859ddcb298fadf90e4ee026819e592
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-13 08:27:40 +0900

    :tada:useSelector use

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+	const counter = useSelector((state) => state.counter);
+	const isLogin = useSelector((state) => state.isLogin);
+
 	return (
 		<div className="App">
 			<h1>Hello</h1>
+			<h3>Count: {counter}</h3>
+			{isLogin ? <h3>ログインに成功</h3> : <h3>ログインしてください</h3>}
 		</div>
 	);
 }
