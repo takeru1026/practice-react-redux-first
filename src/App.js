30d56059c2e96e5e7d34ab7f297b8bc45d0411c1
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-13 08:29:49 +0900

    :construction:WIP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
 	const counter = useSelector((state) => state.counter);
 	const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
 
 	return (
 		<div className="App">
 			<h1>Hello</h1>
 			<h3>Count: {counter}</h3>
+      <button onClick={()=>dispatch() }>+</button>
 			{isLogin ? <h3>ログインに成功</h3> : <h3>ログインしてください</h3>}
 		</div>
 	);
