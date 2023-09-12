5e33b7309af871774eec9953f8eddcd839f57c85
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-13 08:20:16 +0900

    :tada:add dev tools

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import { createStore } from 'redux';
 import allReducers from './reducers';
 
-const store = createStore(allReducers);
+const store = createStore(
+	allReducers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
