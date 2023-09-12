c9159dba510b39f02a735cf8d07495c8b83032b0
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-13 08:23:23 +0900

    :tada:add provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(
 	allReducers,
@@ -12,6 +13,8 @@ const store = createStore(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
