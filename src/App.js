86bdef2fb3fd74bda1ff011aaee799177b843899
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-09-14 08:06:07 +0900

    :tada:fin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment, login } from './actions';
 
 function App() {
 	const counter = useSelector((state) => state.counter);
 	const isLogin = useSelector((state) => state.isLogin);
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
 	return (
 		<div className="App">
 			<h1>Hello</h1>
 			<h3>Count: {counter}</h3>
-      <button onClick={()=>dispatch() }>+</button>
+			<button onClick={() => dispatch(increment(7))}>+</button>
+			<button onClick={() => dispatch(decrement())}>-</button>
 			{isLogin ? <h3>ログインに成功</h3> : <h3>ログインしてください</h3>}
+			<button onClick={() => dispatch(login())}>ログイン OR ログアウト</button>
 		</div>
 	);
 }
